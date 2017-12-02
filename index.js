b6f387f73fd67da281afbdfd28b22219455ff08d
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-02 20:30:23 +0700

    a bcd

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello</h1>');
+});
+
+app.get('/chao/:name', (req, res) => {
+    const name = req.params.name;
+    res.send('Chao ' + name);
+});
+
+app.get('/tinh/:tenPhepTinh/:soA/:soB', (req, res) => {
+    const { tenPhepTinh, soA, soB } = req.params;
+    const pt = new PhepTinh(tenPhepTinh, soA, soB);
+    res.send(pt.getResultString());
+});
+
+// localhost:3000/CONG/4/5 -> '4 + 5 = 9'
+
+app.get('/x', (req, res) => res.send('HIHIHIHI'));
+app.get('*', (req, res) => res.send('404 Not found'));
+app.listen(3000, () => console.log('Server started'));
+
+class PhepTinh {
+    constructor(tenPhepTinh, soA, soB) {
+        this.tenPhepTinh = tenPhepTinh;
+        this.soA = soA;
+        this.soB = soB;
+    }
+    getResultString() {
+        const calString = this.getCalString();
+        return `${calString} = ${eval(calString)}`
+    }
+    getCalString() {
+        const { tenPhepTinh, soA, soB } = this;
+        if (tenPhepTinh === 'CONG') return `${soA} + ${soB}`;
+        if (tenPhepTinh === 'TRU') return `${soA} - ${soB}`;
+        if (tenPhepTinh === 'NHAN') return `${soA} * ${soB}`;
+        return `${soA} / ${soB}`;
+    }
+}
